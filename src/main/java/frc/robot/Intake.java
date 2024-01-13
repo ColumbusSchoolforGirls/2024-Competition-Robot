265aea1b5a31e2f1c0c327bd901f43ac364d9772
Team 677 <firstteam677@columbusschoolforgirls.org>
2024-01-13 14:35:20 -0500

    code for calculating the ground distance to the speaker

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -7,6 +7,23 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Intake {
+    public Limelight limelight;
+
+    double targetOffsetAngle_Vertical = limelight.ty.getDouble(0.0);
+    double limelightMountAngleDegrees = 45.0; //will need to change
+    double limelightLensHeight = 6.0; //in inches -- will need to change
+    //distance from center of the limelight lens tot he floor
+    double goalHeight = 24.0; //might need to change // in inches //goal = april tag NOT speaker
+    //this is for the speaker
+    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+    double distanceFromGroundToAprilTag = 57.125;
+
+    double distanceFromLimelightToGoal = (goalHeight - limelightLensHeight) / Math.tan(angleToGoalRadians);
+    //calculates distance
+    double groundDistanceToSpeaker = Math.sqrt(Math.pow(distanceFromLimelightToGoal, 2) - Math.pow(distanceFromGroundToAprilTag, 2));
+
 //default intake (arm down)
 //1 motor to drive arm up
 //activate intake, move arm up, hold in place using other "intake", speed up Shooter, move note using Holder
