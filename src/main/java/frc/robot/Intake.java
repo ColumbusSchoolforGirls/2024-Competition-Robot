b4a9f0ff127cd0fb2b4cff7f5b174c90dace0e39
lschroeder07 <lschroeder@columbusschoolforgirls.org>
2024-01-13 13:14:41 -0500

    added intake with notes

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -1,5 +1,20 @@
 package frc.robot;
 
-public class Intake {
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Intake {
+//default intake (arm down)
+//1 motor to drive arm up
+//activate intake, move arm up, hold in place using other "intake", speed up Shooter, move note using Holder
+//square to SPEAKER using apriltag
+//trig the angle and speed to shoot
+
+//button to activate INTAKE (not dragging, slightly off ground) **IF has note THEN arm up (parallel with robot chassis)**
+// 
+
+//mech will decide locking mechanism so arm can stop at multiple angles
 }
